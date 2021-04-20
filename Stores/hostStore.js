5444e401ffdd6727250d2ebb9611843a781e38c8
alomaryam <alomaryam@gmail.com>
2021-04-20 22:36:31 +0300

    all stores fixed

--- Stores/hostStore.js
+++ Stores/hostStore.js
@@ -3,12 +3,11 @@ import instance from "./instance";
 
 // What we need CRUD
 
-// ********NOTE********
-// will change all routes based on the backend
+// ***NOTE***
+// all routes fixed!
 
 class HostStore {
   hosts = [];
-
   loading = true;
 
   fetchHosts = async () => {
@@ -21,30 +20,35 @@ class HostStore {
     }
   };
 
+  // *** Note ***
+  // Delete Host Route was: /users/petHosts/${petHostId} // *** removed petHostId  ***
+
   deleteHost = async (petHostId) => {
     try {
-      await instance.delete(`/users/petHosts/${petHostId}`);
+      await instance.delete(`/users/petHosts`);
       this.hosts = this.hosts.filter((host) => host.id !== petHostId);
     } catch (error) {
       console.error("HostStore -> deleteHost -> error", error);
     }
   };
+
   //after signing up we create a pet-owner profile
+
   createHost = async (data) => {
     try {
-      const response = await instance.post(
-        `/users/petHosts`,
-        data
-      );
+      const response = await instance.post(`/users/petHosts`, data);
       this.hosts.push(response.data);
     } catch (error) {
       console.error("HostStore -> createHost -> error", error);
     }
   };
 
+  // *** Note ***
+  // Update Host Route was: /users/petHosts/${petHostId} // *** removed petHostId  ***
+
   updateHost = async (petHostId) => {
     try {
-      await instance.put(`/users/petHosts/${petHostId}`);
+      await instance.put(`/users/petHosts`);
       this.hosts = this.hosts.filter((host) => host.id !== petHostId);
     } catch (error) {
       console.error("HostStore -> updateHost -> error", error);
