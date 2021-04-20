ab4a22fbf7fc0bfcfe5d4f111463a1509e325dde
SMbinSalem <78157317+Smbinsalem@users.noreply.github.com>
2021-04-20 06:03:27 +0300

    Added a calender on Sign Up page that needs to be fixed for the backend usage

--- src/Screens/SignUp.js
+++ src/Screens/SignUp.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { View, ScrollView } from "react-native";
 import authStore from "../../Stores/authStore";
 import { useState } from "react";
-import { Icon } from "native-base";
+import { Icon, Label } from "native-base";
 import styled from "styled-components/native";
-import { Calendar } from "react-native-calendars";
 import DropDownPicker from "react-native-dropdown-picker";
+
+//Calendar
+import { Calendar } from "react-native-calendars";
+
 //Images
 import Pet1 from "../../assets/images/Pet8.jpeg";
+
+const theme = {
+  Maincolor: "#9381ff", // redish main font color
+  backgroundColor: "#f5fffa", // white main background color
+  black: "black",
+  blackish: "#484848",
+  grey: "#99aab5",
+  blueish: "#9381ff",
+  redish: "#f23d3a",
+};
 
 const SignUp = ({ navigation }) => {
   // if (authStore.user) navigation.replace("Home");
@@ -22,78 +36,160 @@ const SignUp = ({ navigation }) => {
     password: "",
   });
 
-  const handleSubmit =  () => authStore.signup(user, navigation);
+  const handleSubmit = () => authStore.signup(user, navigation);
 
   return (
     <>
       <BackgroundIMG source={Pet1}>
-        <AuthContainer>
-          <AuthTextInput
-            placeholder="First Name"
-            placeholderTextColor="white"
-            onChangeText={(firstName) => setUser({ ...user, firstName })}
-          />
-          <AuthTextInput
-            placeholder="Last Name"
-            placeholderTextColor="white"
-            onChangeText={(lastName) => setUser({ ...user, lastName })}
-          />
-          <AuthTextInput
-            placeholder="Email"
-            autoCapitalize="none"
-            placeholderTextColor="white"
-            onChangeText={(email) => setUser({ ...user, email })}
-          />
-          <AuthTextInput
+        <ScrollView>
+          <AuthContainer>
+            <AuthTextInput
+              placeholder="First Name"
+              placeholderTextColor="white"
+              onChangeText={(firstName) => setUser({ ...user, firstName })}
+            />
+            <AuthTextInput
+              placeholder="Last Name"
+              placeholderTextColor="white"
+              onChangeText={(lastName) => setUser({ ...user, lastName })}
+            />
+            <AuthTextInput
+              placeholder="Email"
+              autoCapitalize="none"
+              placeholderTextColor="white"
+              onChangeText={(email) => setUser({ ...user, email })}
+            />
+            {/* <AuthTextInput
             placeholder="Date of Birth"
             placeholderTextColor="white"
             onChangeText={(dateOfBirth) => setUser({ ...user, dateOfBirth })}
-          />
-          <AuthTextInput
-            placeholder="Contact Number"
-            placeholderTextColor="white"
-            onChangeText={(contactNumber) =>
-              setUser({ ...user, contactNumber })
-            }
-          />
-          {/* <AuthTextInput
+          /> */}
+            <Label>Date Of Birth</Label>
+            <Calendar
+              minDate={"1921-01-01"}
+              // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+              maxDate={"2021-12-31"}
+              // Handler which gets executed on day press. Default = undefined
+              onDayPress={(value) => {
+                setUser({
+                  ...user,
+                  dateOfBirth: value.dateString, //***it was value but i added .dateString cuz of BE datatype issue*** BY SAlWA
+                }),
+                  console.log("selected day", value);
+              }}
+              markedDates={{
+                [user.dateOfBirth]: {
+                  selected: true,
+                  disableTouchEvent: true,
+                  selectedDotColor: "orange",
+                },
+              }}
+              // Handler which gets executed on day long press. Default = undefined
+              onDayLongPress={(day) => {
+                console.log("selected long press day", day);
+              }}
+              // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+              monthFormat={"yyyy-MM-dd"}
+              // Handler which gets executed when visible month changes in calendar. Default = undefined
+              onMonthChange={(month) => {
+                console.log("month changed", month);
+              }}
+              // Do not show days of other months in month page. Default = false
+              hideExtraDays={true}
+              // day from another month that is visible in calendar page. Default = false
+              disableMonthChange={true}
+              // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+              firstDay={1}
+              // Hide day names. Default = false
+              hideDayNames={false}
+              // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+              onPressArrowLeft={(subtractMonth) => subtractMonth()}
+              // Handler which gets executed when press arrow icon right. It receive a callback can go next month
+              onPressArrowRight={(addMonth) => addMonth()}
+              // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
+              disableAllTouchEventsForDisabledDays={true}
+              // Enable the option to swipe between months. Default = false
+              enableSwipeMonths={true}
+              // Specify style for calendar container element. Default = {}
+              style={{
+                borderWidth: 1,
+                borderColor: "gray",
+                height: 300,
+              }}
+              // Specify theme properties to override specific styles for calendar parts. Default = {}
+              theme={{
+                selectedColor: theme.Maincolor,
+                backgroundColor: "#ffffff",
+                calendarBackground: "#ffffff",
+                textSectionTitleColor: theme.Maincolor,
+                textSectionTitleDisabledColor: "#d9e1e8",
+                selectedDayBackgroundColor: theme.Maincolor,
+                selectedDayTextColor: "#ffffff",
+                todayTextColor: theme.Maincolor,
+                dayTextColor: "#2d4150",
+                textDisabledColor: "#d9e1e8",
+                dotColor: theme.Maincolor,
+                selectedDotColor: theme.Maincolor,
+                arrowColor: theme.Maincolor,
+                disabledArrowColor: "#d9e1e8",
+                monthTextColor: theme.blackish,
+                indicatorColor: theme.blackish,
+                // textDayFontFamily: "HelveticaNeue-Medium",
+                // textMonthFontFamily: "HelveticaNeue-Medium",
+                // textDayHeaderFontFamily: "HelveticaNeue-Medium",
+                textDayFontWeight: "300",
+                textMonthFontWeight: "bold",
+                textDayHeaderFontWeight: "300",
+                textDayFontSize: 16,
+                textMonthFontSize: 16,
+                textDayHeaderFontSize: 16,
+              }}
+            />
+            <AuthTextInput
+              placeholder="Contact Number"
+              placeholderTextColor="white"
+              onChangeText={(contactNumber) =>
+                setUser({ ...user, contactNumber })
+              }
+            />
+            {/* <AuthTextInput
             placeholder="Gender"
             placeholderTextColor="white"
             onChangeText={(gender) => setUser({ ...user, gender })}
           /> */}
-          
-          {/* Ex */}
-          <DropDownPicker
-        items={[
-          { label: "Female", value: "Female" },
-          { label: "Male", value: "Male" },
-        
-        ]}
-        placeholder="Gender"
-        containerStyle={{ height: 40, width: 320}}
-        onChangeItem={(item) => setUser({ ...user, gender: item.value })}
-      />
-          <AuthTextInput
-            placeholder="Username"
-            autoCapitalize="none"
-            placeholderTextColor="white"
-            onChangeText={(username) => setUser({ ...user, username })}
-          />
-          <AuthTextInput
-            placeholder="Password"
-            autoCapitalize="none"
-            placeholderTextColor="white"
-            secureTextEntry={true}
-            onChangeText={(password) => setUser({ ...user, password })}
-          />
 
-          <AuthButton onPress={handleSubmit}>
-            <AuthButtonText>Next</AuthButtonText>
-          </AuthButton>
-          <AuthOther onPress={() => navigation.navigate("SignIn")}>
-            Click here if you already have an account!
-          </AuthOther>
-        </AuthContainer>
+            {/* Ex */}
+            <DropDownPicker
+              items={[
+                { label: "Female", value: "Female" },
+                { label: "Male", value: "Male" },
+              ]}
+              placeholder="Gender"
+              containerStyle={{ height: 40, width: 320 }}
+              onChangeItem={(item) => setUser({ ...user, gender: item.value })}
+            />
+            <AuthTextInput
+              placeholder="Username"
+              autoCapitalize="none"
+              placeholderTextColor="white"
+              onChangeText={(username) => setUser({ ...user, username })}
+            />
+            <AuthTextInput
+              placeholder="Password"
+              autoCapitalize="none"
+              placeholderTextColor="white"
+              secureTextEntry={true}
+              onChangeText={(password) => setUser({ ...user, password })}
+            />
+
+            <AuthButton onPress={handleSubmit}>
+              <AuthButtonText>Next</AuthButtonText>
+            </AuthButton>
+            <AuthOther onPress={() => navigation.navigate("SignIn")}>
+              Click here if you already have an account!
+            </AuthOther>
+          </AuthContainer>
+        </ScrollView>
       </BackgroundIMG>
     </>
   );
@@ -109,7 +205,8 @@ export const BackgroundIMG = styled.ImageBackground`
   justify-content: center;
 `;
 export const AuthContainer = styled.View`
-  flex: 2;
+  flex: 1;
+  padding-top: 27%;
   align-self: stretch;
   justify-content: center;
   align-items: center;
@@ -142,15 +239,15 @@ export const AuthButton = styled.TouchableOpacity`
   background-color: #f0ba00;
   margin-top: 30px;
   border-top-left-radius: 30px;
-  border-Bottom-Left-Radius: 30px;
-  border-Bottom-Right-Radius: 30px;
-  border-Top-Right-Radius: 30px;
+  border-bottom-left-radius: 30px;
+  border-bottom-right-radius: 30px;
+  border-top-right-radius: 30px;
 `;
 
 export const AuthButtonText = styled.Text`
   color: #fcfdff;
   font-weight: bold;
-  font-size: 18px;           
+  font-size: 18px;
 `;
 
 export const AuthOther = styled.Text`
