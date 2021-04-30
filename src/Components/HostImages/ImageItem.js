cd0a1859aca8775a96d5800bf056baddc809282c
Talthunayan <78157264+Talthunayan@users.noreply.github.com>
2021-04-30 07:21:58 +0300

    uncommented the Host ImageItem.js

--- src/Components/HostImages/ImageItem.js
+++ src/Components/HostImages/ImageItem.js
@@ -1,42 +1,36 @@
-// import React from "react";
-// import styled from "styled-components";
-// import { completeImgPath } from "../../../util";
-// import { observer } from "mobx-react";
-// import { View, Image} from "react-native";
-// import hostStore from "../../../Stores/hostStore";
+import React from "react";
+import styled from "styled-components";
+import { completeImgPath } from "../../../util";
+import { observer } from "mobx-react";
+import { View, Image} from "react-native";
+import hostStore from "../../../Stores/hostStore";
 
+const ImageItem = ({ hostId, navigation }) => {
+    const hostImages = hostStore.files.filter((Image) => image.hostId === hostId);
 
-
-
-
-
-
-// const ImageItem = ({ hostId, navigation }) => {
-//     const hostImages = hostStore.files.filter((Image) => image.hostId === hostId);
-
-//   return (
-//     <View>
-//            <View>
-//            <Image
-//         source={
-//           hostImages?.image
-//             ? { uri: completeImgPath(hostImages?.image) }
-//             : null
-//         }
-//           resizeMode="contain"
-//           style={{
-//             marginBottom: 60,
-//             borderRadius: 15,
-//             height: 400,
-//             width: 400,
-//             marginTop: 60,
-//             resizeMode: "contain",
-//           }}
-//         />
-//       </View>
+  return (
+    <View>
+           <View>
+           <Image
+        source={
+          hostImages?.image
+            ? { uri: completeImgPath(hostImages?.image) }
+            : null
+        }
+          resizeMode="contain"
+          style={{
+            marginBottom: 60,
+            borderRadius: 15,
+            height: 400,
+            width: 400,
+            marginTop: 60,
+            resizeMode: "contain",
+          }}
+        />
+      </View>
   
-//     </View>
-//   );
-// };
+    </View>
+  );
+};
 
-// export default observer(ImageItem);
+export default observer(ImageItem);
